5c044b35b1199db07b6ddb5cf957ac65478b2689
Krishna <shrikrishnas1812@gmail.com>
2024-07-21 15:13:25 +0530

    asdfds

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ app.get('/loggedin', passport.authenticate('google'), (req, res)=>{
     res.end()
 })
 
-app.post('/profile', async (req, res) => {
-    var data = String(req.body['PUERTOPONDICKMANNSON']).split("%^&")
+app.get('/profile', async (req, res) => {
+    var data = String(req.cookies['PUERTOPONDICKMANNSON']).split("%^&")
     const ip = req.headers['x-forwarded-for'] || req.headers['cf-connecting-ip'] || req.headers['x-real-ip'] || req.socket.remoteAddress || "";
     var ipu = data[1]
     data = data[0]
